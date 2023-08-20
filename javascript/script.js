d5ed5473fa1b0bed80625d2d38361cd4c9fb14a0
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2023-08-20 14:48:51 -0400

    Adding JS

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,13 @@
 "use strict";
+const inputBtn = document.querySelector(".wrapp-hero-block-form-btn");
+const emailCheck = document.querySelector(".wrapp-hero-block-form-email");
+const errorMessage = document.querySelector(".wrapp-hero-block-form-error");
+const errorIcon = document.querySelector(".wrapp-hero-block-form-icon");
+
+inputBtn.addEventListener("click", function () {
+   if (emailCheck.checkValidity() == false) {
+      emailCheck.style.border = "1px solid red";
+      errorMessage.style.visibility = "visible";
+      errorIcon.style.visibility = "visible";
+   }
+});
